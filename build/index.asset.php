d1638d525e54c6c6a310e27bf076fe4bb9250e9f
crispacfo <136741510+crispacfo@users.noreply.github.com>
2026-09-19 18:38:20 -0700

    Add production build
    Add compiled JavaScript, styles and WordPress asset metadata for version 1.0.0.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-preferences', 'wp-primitives', 'wp-rich-text'), 'version' => 'ac712cc3811a7da77bc2');
